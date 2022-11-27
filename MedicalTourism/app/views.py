22f7b764d9e695d106d4163439c7422c5d0fc54e
Rohit Jha <rohitj.ug19.cs@nitp.ac.in>
2022-11-28 00:21:11 +0530

    Added functionality of locating hospitals and doctors

--- MedicalTourism/app/views.py
+++ MedicalTourism/app/views.py
@@ -13,7 +13,9 @@ import logging
 from neo4j.exceptions import ServiceUnavailable
 
 #For api calling
+import googlemaps
 import requests
+import json
 
 def index(request):
     return render(request, 'index.html')
@@ -77,7 +79,151 @@ def input(request):
     return render(request, 'input.html')
 
 
-def handleInput(request):
+def graph(request):
     if request.method == "POST":
+       disease = request.POST['disease']
+       budget = request.POST['budget']
+       city = request.POST['city']
+       print(city)
 
-       return HttpResponse("Query submitted")
+
+       #traverse knowledge graph to find locations where disease is treated with in budget.
+       uri = "neo4j+s://644b1253.databases.neo4j.io" 
+       user = "neo4j"
+       password = "MCeuOS4sfD2RnrMZ3Qa3Q1on8NciRV_9ueu5Igu1eGA"
+       app = App(uri, user, password)
+       topics = app._returnCitiesUnderBudget(3.1, budget)
+
+       print(topics)
+       locations = '+'.join(topics)
+       if len(locations) == 0:
+         return render(request, "oops.html")
+
+       locations = locations + "+" + city + "+" + disease
+       return redirect("/mapModule/" + locations)
+
+
+def mapModule(request, location):
+    #Googlemaps
+    maps_api_key = 'AIzaSyB4s3s2DGxNxc7Inl12jxHrExJIHoyZIrw'
+    gmaps = googlemaps.Client(key=maps_api_key)
+
+    #Sorting locations based on distance using geolocation API and storing in pair
+    locations = location.split('+')
+    print(locations)
+
+    pair = {}
+    n = len(locations)
+    for ind in range(n-2) :
+        # Requires cities name
+        src_city = locations[n-2]
+        dest_city = locations[ind]
+        my_dist = gmaps.distance_matrix(src_city, dest_city)['rows'][0]['elements'][0]
+
+        # Printing the result
+        print(locations[ind], my_dist['distance']['value'])
+
+        #add to dictionary
+        pair[locations[ind]] = my_dist['distance']['value'] 
+
+
+    #sort pair dictionary
+    pair = sorted(pair.items(), key=lambda kv:(kv[1], kv[0]))
+
+    count=0
+    sortedLocs = ''
+    print("Sorted pair : ")
+    for ind in range(3):
+        if ind < len(pair):
+            sortedLocs +=  "+" + pair[ind][0]
+            count += 1
+            
+
+    return redirect('/display/1+' + str(count) + sortedLocs + "+" + locations[n-1])
+
+
+
+def displayPlaces(request, sortedloc):
+    sortedLocs = sortedloc.split('+')
+
+    display = int(sortedLocs[0])
+    count = int(sortedLocs[1])
+
+    # Locate Hospitals and Doctors using places API.
+    disease = sortedLocs[-1]
+    location = sortedLocs[display+1]
+    url = f"https://maps.googleapis.com/maps/api/place/textsearch/json?query=Hospitals For {disease}%20in%20{location}&key=AIzaSyB4s3s2DGxNxc7Inl12jxHrExJIHoyZIrw"
+
+    payload={}
+    headers = {}
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    result = []
+    for dics in json.loads(response.text)['results'] :
+        result.append((dics['formatted_address'], dics['name'], dics['rating']))
+
+
+
+    print(sortedLocs)
+    countlist = []
+    for ind in range(count):
+       countlist.append(ind+1) 
+
+    # result = [
+    #     ("Department of Orthopaedic, Room NO- 1218A, Sir Ganga Ram Hospital", 
+    #     "Dr D K Dhiraj (Sir Ganga Ram Hospital), Best Orthopaedic Surgeon Delhi, knee specilaist", 
+    #     4.9,
+    #     ),
+    #     ("Department of Orthopaedic, Room NO- 1218A, Sir Ganga Ram Hospital", 
+    #     "Dr D K Dhiraj", 
+    #     4.9,
+    #     ),
+    #     ("Department of Orthopaedic, Room NO- 1218A, Sir Ganga Ram Hospital", 
+    #     "Dr D K Dhiraj", 
+    #     4.9,
+    #     ),
+    #     ("Department of Orthopaedic, Room NO- 1218A, Sir Ganga Ram Hospital", 
+    #     "Dr D K Dhiraj", 
+    #     4.9,
+    #     ),
+    #     ("Department of Orthopaedic, Room NO- 1218A, Sir Ganga Ram Hospital", 
+    #     "Dr D K Dhiraj", 
+    #     4.9,
+    #     ),
+    #     ("Department of Orthopaedic, Room NO- 1218A, Sir Ganga Ram Hospital", 
+    #     "Dr D K Dhiraj", 
+    #     4.9,
+    #     )
+
+    #     ]
+
+    return render(request, 'output.html', {'sortedloc' : sortedloc[2:], 'countlist':countlist, 'location': sortedLocs[display + 1].capitalize(), 'result' : result})
+    
+
+
+class App:
+
+    def __init__(self, uri, user, password):
+        self.driver = GraphDatabase.driver(uri, auth=(user, password))
+
+    def close(self):
+        # Don't forget to close the driver connection when you are finished with it
+        self.driver.close()
+
+
+    def _returnCitiesUnderBudget(self,nodeid,budget):
+        with self.driver.session() as session:
+            result = session.read_transaction(self._returnCities,nodeid,budget)
+            cities = []
+            for row in result:
+                cities.append("{row}".format(row=row))
+            return cities
+
+    @staticmethod
+    def _returnCities(tx,nodeid,budget):
+         query = (
+            "MATCH (l:Loc) WHERE l.maxPrice <= {budget} AND l.parent_id={nodeid} RETURN l.name AS name".format(budget=budget, nodeid=nodeid)
+        ) 
+         result = tx.run(query, nodeid=nodeid,budget=budget)
+         return [row["name"] for row in result]
